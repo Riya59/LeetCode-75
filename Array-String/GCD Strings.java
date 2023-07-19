86d41a1d602e3ba670662e6ca3e03df3ed51ac0c
Riya Verma <77240974+Riya59@users.noreply.github.com>
2023-07-19 16:48:13 +0530

    Create GCD Strings.java
    Greatest Common Divisor of Strings

---
+++ Array-String/GCD Strings.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String gcdOfStrings(String str1, String str2) {
+        //checking if concatenated string is equal or not , if not then return empty string
+        if(!(str1+str2).equals(str2+str1)){
+            return "";
+        }
+        // if concatenated string is equal
+        int gcd = gcd(str1.length(), str2.length());
+        return str1.substring(0,gcd);
+    }
+
+    private int gcd(int a, int b){
+        return b==0?a: gcd(b, a%b);
+    }
+
+}
